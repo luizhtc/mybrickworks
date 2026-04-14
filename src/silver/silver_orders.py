46daa85b43872408ba3c019b8be3072ec0c43693
luizhtc <luizhtrincac@gmail.com>
2026-04-14 02:40:44 +0000

    refactor: added utils file with silver notebooks

--- src/silver/silver_orders.py
+++ src/silver/silver_orders.py
@@ -1,37 +1,7 @@
 # Databricks notebook source
 from pyspark.sql import DataFrame, Column
-from pyspark.sql.functions import sum as _sum, max as _max, collect_set, col, to_timestamp, coalesce, datediff, when
-
-# COMMAND ----------
-
-def read_from_bronze(table: str) -> DataFrame:
-    """Reads a table from the Catalog 'cat_olist' and Schema 'sch_bronze'
-
-    Args:
-        table (string): The table name to be read.
-
-    Returns:
-        DataFrame: The PySpark DataFrame loaded with the table contents.
-    """
-    CATALOG = "cat_olist"
-    SCHEMA = "sch_bronze"
-
-    df = spark.read.table(
-        f"{CATALOG}.{SCHEMA}.{table}"
-    )
-    return df
-
-def parse_timestamp(column: Column):
-    """Parses a string formatted column into a timestamp column given formats
-
-    Args:
-        column (Column): The column to be parsed.
-
-    Returns:
-        Column: The parsed column with the timestamp information or NULL in case of parsing failure.
-    """
-    FORMATS = ["yyyy-MM-dd HH:mm:ss"]
-    return coalesce(*[to_timestamp(column, fmt) for fmt in FORMATS])
+from pyspark.sql.functions import sum as _sum, max as _max, collect_set, col, datediff, when
+from utils import read_from_bronze, parse_timestamp
 
 # COMMAND ----------
 
---
+++ src/silver/utils.py
@@ -0,0 +1,35 @@
+from pyspark.sql import DataFrame, Column, SparkSession
+from pyspark.sql.functions import coalesce, to_timestamp
+
+def read_from_bronze(table: str, spark: SparkSession | None = None) -> DataFrame:
+    """Reads a table from the Catalog 'cat_olist' and Schema 'sch_bronze'
+
+    Args:
+        table (string): The table name to be read.
+
+    Returns:
+        DataFrame: The PySpark DataFrame loaded with the table contents.
+    """
+    spark = spark or SparkSession.getActiveSession()
+    if spark is None:
+        raise RuntimeError("No active SparkSession found.")
+    
+    CATALOG = "cat_olist"
+    SCHEMA = "sch_bronze"
+
+    df = spark.read.table(
+        f"{CATALOG}.{SCHEMA}.{table}"
+    )
+    return df
+
+def parse_timestamp(column: Column):
+    """Parses a string formatted column into a timestamp column given formats
+
+    Args:
+        column (Column): The column to be parsed.
+
+    Returns:
+        Column: The parsed column with the timestamp information or NULL in case of parsing failure.
+    """
+    FORMATS = ["yyyy-MM-dd HH:mm:ss"]
+    return coalesce(*[to_timestamp(column, fmt) for fmt in FORMATS])
