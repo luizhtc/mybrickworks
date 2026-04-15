6971ed62fa2cd3832621ffbe9c197b9818f4e2bc
Luiz Henrique Trinca Caldo <43818123+luizhtc@users.noreply.github.com>
2026-04-15 14:19:48 -0300

    feat: order_items silver table notebook (#4)

---
+++ src/silver/order_items.py
@@ -0,0 +1,42 @@
+# Databricks notebook source
+from utils import read_from_bronze
+from pyspark.sql.functions import col
+
+# COMMAND ----------
+
+order_items = read_from_bronze("tbl_olist_order_items_dataset")
+sellers = read_from_bronze("tbl_olist_sellers_dataset")
+
+# COMMAND ----------
+
+order_items_join =\
+order_items.join(
+    sellers,
+    on="seller_id",
+    how="left"
+)
+
+# COMMAND ----------
+
+order_items_transformed =\
+order_items_join.select(
+    "order_id",
+    col("order_item_id").cast("int"),
+    "product_id",
+    col("price").cast("double"),
+    col("freight_value").cast("double"),
+    "seller_id",
+    "seller_city",
+    "seller_state",
+    "seller_zip_code_prefix"
+)
+
+# COMMAND ----------
+
+(
+    order_items_transformed
+        .write
+        .format("delta")
+        .mode("overwrite")
+        .saveAsTable(f"`cat_olist`.`sch_silver`.`order_items`")
+)
