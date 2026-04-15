6934d27c4822a4c77e0f37ca7667c71e2fbd89bd
luizhtc <luizhtrincac@gmail.com>
2026-04-15 17:56:16 +0000

    feat: sales_by_category

---
+++ src/gold/sales_by_category.py
@@ -0,0 +1,60 @@
+# Databricks notebook source
+from utils import read_from_silver
+from pyspark.sql import Window
+from pyspark.sql.functions import sum as _sum, countDistinct, count as _count, avg, col, rank
+
+# COMMAND ----------
+
+orders = read_from_silver("orders")
+order_items = read_from_silver("order_items")
+products = read_from_silver("products")
+
+# COMMAND ----------
+
+orders_and_products =\
+order_items.join(
+    products,
+    on="product_id",
+    how="left"
+).join(
+    orders,
+    on="order_id",
+    how="left"
+)
+
+# COMMAND ----------
+
+orders_and_products_filtered = orders_and_products.filter("order_status = 'delivered'")
+
+# COMMAND ----------
+
+sales_by_category =\
+orders_and_products_filtered.groupBy(
+    "product_category_name_english"
+).agg(
+    _sum("price").alias("total_revenue"),
+    countDistinct("order_id").alias("total_orders"),
+    _count("*").alias("total_items_sold"),
+    (_sum("price") / countDistinct("order_id")).alias("avg_order_ticket"),
+    avg("price").alias("avg_item_price")
+)
+
+# COMMAND ----------
+
+wn_revenue = Window.orderBy(col("total_revenue").desc(), col("avg_order_ticket").desc())
+
+sales_by_category_ranked =\
+sales_by_category.withColumn(
+    "revenue_rank",
+    rank().over(wn_revenue)
+)
+
+# COMMAND ----------
+
+(
+    sales_by_category_ranked
+        .write
+        .format("delta")
+        .mode("overwrite")
+        .saveAsTable(f"`cat_olist`.`sch_gold`.`sales_by_category`")
+)
---
+++ src/gold/utils.py
@@ -0,0 +1,35 @@
+from pyspark.sql import DataFrame, Column, SparkSession
+from pyspark.sql.functions import coalesce, try_to_timestamp, lit
+
+def read_from_silver(table: str, spark: SparkSession | None = None) -> DataFrame:
+    """Reads a table from the Catalog 'cat_olist' and Schema 'sch_silver'
+
+    Args:
+        table (string): The table name to be read.
+
+    Returns:
+        DataFrame: The PySpark DataFrame loaded with the table contents.
+    """
+    spark = spark or SparkSession.getActiveSession()
+    if spark is None:
+        raise RuntimeError("No active SparkSession found.")
+    
+    CATALOG = "cat_olist"
+    SCHEMA = "sch_silver"
+
+    df = spark.read.table(
+        f"{CATALOG}.{SCHEMA}.{table}"
+    )
+    return df
+
+def parse_timestamp(column: Column):
+    """Parses a string formatted column into a timestamp column given formats
+
+    Args:
+        column (Column): The column to be parsed.
+
+    Returns:
+        Column: The parsed column with the timestamp information or NULL in case of parsing failure.
+    """
+    FORMATS = ["yyyy-MM-dd HH:mm:ss"]
+    return coalesce(*[try_to_timestamp(column, lit(fmt)) for fmt in FORMATS])
