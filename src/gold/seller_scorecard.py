2b96b28b0ac22a16e1868b9ba3df26d9b12ebf3e
luizhtc <luizhtrincac@gmail.com>
2026-05-12 23:57:42 +0000

    temp: seller scorecard updates

--- src/gold/seller_scorecard.py
+++ src/gold/seller_scorecard.py
@@ -1,18 +1,33 @@
 # Databricks notebook source
 from utils import read_from_silver
-from pyspark.sql.functions import col, countDistinct, sum as _sum
+from pyspark.sql.functions import col, countDistinct, sum as _sum, count as _count
 
 # COMMAND ----------
 
-orders = read_from_silver("order")
+orders = read_from_silver("orders")
 order_items = read_from_silver("order_items")
 reviews = read_from_silver("reviews")
+
+# COMMAND ----------
+
+valid_sellers =\
+orders.filter(col("order_status") == "delivered").join(
+    order_items,
+    on="order_id",
+    how="left"
+).groupBy("seller_id").agg(
+    countDistinct(col("order_id")).alias("total_delivered_orders")
+).filter(col("total_delivered_orders") >= 20).select("seller_id").distinct()
 
 # COMMAND ----------
 
 order_items.groupby("seller_id","seller_state").agg(
     countDistinct(col("order_id")).alias("total_orders"),
     _sum(col("price")).alias("total_revenue")
+).join(
+    valid_sellers,
+    on="seller_id",
+    how="inner"
 ).display()
 
 # COMMAND ----------
@@ -21,4 +36,20 @@ reviews.display()
 
 # COMMAND ----------
 
+order_seller = order_items.select("order_id","seller_id").distinct()
 
+# COMMAND ----------
+
+order_seller.join(
+    orders,
+    on="order_id",
+    how="inner"
+).display()
+
+# COMMAND ----------
+
+orders.join(
+    reviews,
+    on="order_id",
+    how="inner"
+).display()
