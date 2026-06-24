7bb767b42b62804fe64c4ed1ef75f8e67f61e21a
luizhtc <luizhtrincac@gmail.com>
2026-06-24 20:23:29 +0000

    feat: download_datasets.py

--- bundles/olist_lakehouse/notebooks/kaggle/download_datasets.py
+++ bundles/olist_lakehouse/notebooks/kaggle/download_datasets.py
@@ -8,6 +8,11 @@ import kagglehub
 # Get Kaggle API Token
 os.environ["KAGGLE_API_TOKEN"] = dbutils.secrets.get("kaggle", "KAGGLE_API_TOKEN")
 # Download to driver
-datasets_path = kagglehub.dataset_download("olistbr/brazilian-ecommerce")
-# Move to volume
-dbutils.fs.cp(datasets_path, "/Volumes/cat_olist/sch_bronze/vol_landing/olist_kaggle_datasets/", recurse=True)
+datasets_path = kagglehub.dataset_download(
+    "olistbr/brazilian-ecommerce",
+    output_dir="/Volumes/cat_olist/sch_bronze/vol_landing/olist_kaggle_datasets/"
+)
+
+# COMMAND ----------
+
+print(f"Downloaded successfully to: \n{datasets_path}")
